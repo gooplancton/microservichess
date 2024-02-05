e462147b90a271b29cd9a7261673c97fbe9aec9f
gooplancton <gooplancton@outlook.com>
2024-02-06 00:22:58 +0100

    added VITE_PUBLIC_API_DOMAIN

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -12,11 +12,19 @@ import { GamePage } from "./pages/game";
 import { JoinPage } from "./pages/join";
 import Cookies from "js-cookie";
 
+interface ImportMetaEnv {
+  readonly VITE_PUBLIC_API_DOMAIN: string
+}
+
+interface ImportMeta {
+  readonly env?: ImportMetaEnv
+}
+
+const VITE_PUBLIC_API_DOMAIN = (import.meta as ImportMeta).env?.VITE_PUBLIC_API_DOMAIN ?? "localhost:8080/trpc"
 
 const wsClient = createWSClient({
-  url: `ws://localhost:8080/trpc`,
+  url: `ws://${VITE_PUBLIC_API_DOMAIN}`,
 });
-
 
 const queryClient = new QueryClient()
 const trpcClient = trpc.createClient({
@@ -26,7 +34,7 @@ const trpcClient = trpc.createClient({
       condition: (op) => op.type === "subscription",
       true: wsLink({ client: wsClient }),
       false: httpBatchLink({
-        url: "http://localhost:8080/trpc",
+        url: `http://${VITE_PUBLIC_API_DOMAIN}`,
         headers() {
           const jwt = Cookies.get("microservichess-user-jwt");
           if (jwt) return { authorization: `Bearer ${jwt}` };
